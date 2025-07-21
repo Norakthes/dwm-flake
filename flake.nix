f4cb3269885cd3b239c47a386e955359fda6076b
Rasmus Bødker <rasmus.b.bodker@gmail.com>
2025-07-22 00:47:52 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Custom DWM build";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    packages.${system}.default = pkgs.dwm.overrideAttrs (oldAttrs: {
+      patches = [];
+
+      postpatch = ''
+        cp ${./config.h} config.h
+      '';
+    });
+  };
+}
